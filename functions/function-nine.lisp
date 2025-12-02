194263610a4012516e4361f4aea80df2d1c4d001
itsyourGOAT <123419631+itsyourGOAT@users.noreply.github.com>
2025-12-01 19:48:14 -0700

    Update function-nine.lisp

--- functions/function-nine.lisp
+++ functions/function-nine.lisp
@@ -1,22 +1,3 @@
-#|
-Goal: Implement the merge sorting algorithm on an input list.
-Assumption: All inputs will be a list, that may contain as few as zero values.
-Complication, how do I recognize and react to the < and > characters in the function definition?
-
-Psuedo:
-def merge (list pred)
-    if list length 0 or 1
-        return list
-
-
-    L = sorted first half
-    R = sorted second half
-    out = empty list
-    filter is pred
-
-
-|#
-
 ;; Perform merge sort on the lists.
 ;; Parameters:
 ;;   list: The list to sort
@@ -26,96 +7,93 @@ def merge (list pred)
 ;;     (merge-sort '(2 1 5 0) #'<) => '(0 1 2 5)
 ;;     (merge-sort '(2 1 5 0) #'>) => '(5 2 1 0)
 
-#|
- Tools:
- Predicate Test:
- (equal predicate #'<)
-
- List halves:
- ?
-
- |#
-
 (defun merge-sort (list predicate)
-    (let ((halfL (/ (length list) 2 )))
-        (cond
-            (
-                (= 0 (length list)) (list nil)
-            )
-            (
-                (= 1 (length list)) list
-            )
-            (
-                (> halfL 1)
-                ( merge-x
-                    (merge-sort (list ( firsthalf list halfL ) ) predicate)
-                    (merge-sort ( secondhalf list halfL ) predicate)
-                    predicate
-                )
-            )
-            (
-                (= halfL 1)
-                ( merge-x (car list) (cdr list) predicate )
-            )
-        )
-    )
-)
-
-
+  (labels
+      ((half-length (n)
+         (if (<= n 1)
+             0
+             (+ 1 (half-length (- n 2))))))
+    (let* ((len (length list))
+           (halfL (half-length len)))
+      (cond
+        ;; empty or single element: already sorted
+        ((or (= len 0) (= len 1)) list)
+        (t
+         (merge-x
+          (merge-sort (firsthalf list halfL) predicate)
+          (merge-sort (secondhalf list halfL) predicate)
+          predicate))))))
+          
 (defun firsthalf (a len)
-    (if
-        (<= len 0)
-        ()
-        (cons (first a) (firsthalf (cdr a) (- len 1)) )
-    )
-)
+  (if (<= len 0)
+      nil
+      (cons (first a)
+            (firsthalf (cdr a) (- len 1)))))
 
 (defun secondhalf (a len)
-    (if
-        (<= len 0)
-        (secondhalf (cdr a) (- len 1))
-        (list a)
-    )
-)
+  (if (<= len 0)
+      a
+      (secondhalf (cdr a) (- len 1))))
 
 (defun merge-x (a b pred)
-    (print (first a))
-    (terpri)
-    (if
-        (equal pred #'<)
-        (if
-            (<= (first a) (first b))
-            (cons (first a) (merge-x (cdr a) b pred))
-            (cons (first b) (merge-x (cdr b) a pred))
-        )
-    )
-    (if
-        (>= (first a) (first b))
-        (cons (first a) (merge-x (cdr a) b pred))
-        (cons (first b) (merge-x (cdr b) a pred))
-    )
-)
+  (cond
+    ;; if one side is empty, return the other
+    ((equal a nil) b)
+    ((equal b nil) a)
+    ;; choose smallest (or largest)
+    ((funcall pred (first a) (first b))
+     (cons (first a) (merge-x (cdr a) b pred)))
+    (t
+     (cons (first b) (merge-x a (cdr b) pred)))))
+     
+;; ----------------------------------------------------------------------
+;; Tests for merge-sort
+;; ----------------------------------------------------------------------
 
+(format t "Testing merge-sort:~%~%")
 
+;; ------------------------------------------------------------
+;; Basic numeric ascending tests
+;; ------------------------------------------------------------
+(format t "Numeric ascending (#'<) tests:~%")
+(format t "(merge-sort '(3 1 4 2) #'<) => ~a  (expected: (1 2 3 4))~%"
+        (merge-sort '(3 1 4 2) #'<))
+(format t "(merge-sort '(5 5 3 1 4) #'<) => ~a  (expected: (1 3 4 5 5))~%"
+        (merge-sort '(5 5 3 1 4) #'<))
+(format t "(merge-sort '() #'<) => ~a  (expected: NIL)~%"
+        (merge-sort '() #'<))
+(format t "(merge-sort '(42) #'<) => ~a  (expected: (42))~%"
+        (merge-sort '(42) #'<))
+(format t "(merge-sort '(1 2 3 4) #'<) => ~a  (expected: (1 2 3 4))~%"
+        (merge-sort '(1 2 3 4) #'<))
+(format t "~%")
 
-(defun test-merge-sort (in pred target)
-    (print in)
-    (print pred)
-    (print '_)
-    (let ((out (merge-sort in pred)))
-        (print out)
-        (print (equal out target))
-    )
-)
+;; ------------------------------------------------------------
+;; Numeric descending tests
+;; ------------------------------------------------------------
+(format t "Numeric descending (#'>) tests:~%")
+(format t "(merge-sort '(3 1 4 2) #'>) => ~a  (expected: (4 3 2 1))~%"
+        (merge-sort '(3 1 4 2) #'>))
+(format t "(merge-sort '(5 5 3 1 4) #'>) => ~a  (expected: (5 5 4 3 1))~%"
+        (merge-sort '(5 5 3 1 4) #'>))
+(format t "~%")
 
-#|
-(defun halftests (in)
-    (print (firsthalf in (/ (length in) 2)))
-    (print (secondhalf in (/ (length in) 2)))
-)
-|#
+;; ------------------------------------------------------------
+;; Symbol / string tests
+;; ------------------------------------------------------------
+(format t "Symbol / string tests:~%")
+(format t "(merge-sort '(c a d b) #'char<) => ~a  (expected: (a b c d))~%"
+        (merge-sort '(#\c #\a #\d #\b) #'char<))
+(format t "(merge-sort '(\"pear\" \"apple\" \"banana\") 'string<) => ~a  (expected: (\"apple\" \"banana\" \"pear\"))~%"
+        (merge-sort '(\"pear\" \"apple\" \"banana\") 'string<))
+(format t "~%")
 
-(test-merge-sort '(2 1 5 0) #'< (list 0 1 2 5))
-(test-merge-sort '(2 1 5 0) #'> (list 5 2 1 0))
-
-;(halftests '(2 1 5 0))
+;; ------------------------------------------------------------
+;; Already sorted vs reverse sorted (sanity checks)
+;; ------------------------------------------------------------
+(format t "Already-sorted / reverse-sorted checks:~%")
+(format t "(merge-sort '(1 2 3 4 5 6) #'<) => ~a  (expected: (1 2 3 4 5 6))~%"
+        (merge-sort '(1 2 3 4 5 6) #'<))
+(format t "(merge-sort '(6 5 4 3 2 1) #'<) => ~a  (expected: (1 2 3 4 5 6))~%"
+        (merge-sort '(6 5 4 3 2 1) #'<))
+(format t "~%")
